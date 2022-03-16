fb3ddb7c895ed5550a050a6d62cd6700cb1f3f30
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-15 21:17:17 -0300

    Revert "Merge pull request #1 from BotechiaErika/production"
    This reverts commit 99f9ad34e402ecabeff64076ca48a8faf23f3618, reversing
    changes made to 7bd41ee1373b3d91756f9a6bdada74a6517f4835.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const path = require('path')
 const mainRouter = require('./routes/mainRoutes.js')
 const empresaRouter = require('./routes/empresasRoutes.js')
 const userRouter = require('./routes/usersRoutes.js')
 const productsRouters = require('./routes/productsRoutes.js')
+const express = require('express')
+const path = require('path')
 const app = express()
-require('dotenv').config
+app.use(express.static(path.join(__dirname, './../public')));
+//ACTUALIZANDO ENTRY-POINT CON CLASSE CRUD
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, './../public')));
-app.use(express.static(path.join(__dirname, './../public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/', mainRouter)
 app.use('/empresas', empresaRouter)
 app.use('/usuarios', userRouter)
